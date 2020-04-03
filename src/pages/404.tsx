2ebb7c60d4c59b8a794d530c11bc22a6351bb972
Arash Kadkhodaei <slasharash@protonmail.com>
2020-04-03 19:33:48 +0430

    update not found page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,42 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
+import messages from 'utils/messages'
 import Layout from 'components/Layout'
+import Seo from 'components/Seo'
+import Heading from 'components/Heading'
 
 const NotFoundPage: React.FC = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <Seo slug={'/'} title={messages.pageNotFound} />
+    <Heading>{messages.pageNotFound} :(</Heading>
+    <p>{messages.youCanVisitFollowigPages}</p>
+    <ul>
+      <li className="my-4">
+        <Link
+          to="/"
+          className="text-gold-400 hover:text-gold-600 transition duration-300 ease-in-out"
+        >
+          {messages.mainPage}
+        </Link>
+      </li>
+      <li className="my-4">
+        <Link
+          to="/tags"
+          className="text-gold-400 hover:text-gold-600 transition duration-300 ease-in-out"
+        >
+          {messages.tags}
+        </Link>
+      </li>
+      <li className="my-4">
+        <Link
+          to="/آرش"
+          className="text-gold-400 hover:text-gold-600 transition duration-300 ease-in-out"
+        >
+          {messages.aboutMe}
+        </Link>
+      </li>
+    </ul>
   </Layout>
 )
 
--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -1,10 +1,14 @@
 export default {
+  aboutMe: 'درباره‌ی من',
   arash: 'آرش',
   arashKadkhodaei: 'آرش کدخدائی',
   continueReadig: 'ادمه‌ی نوشته',
   feed: 'فید',
   footerMessage: 'طراحی با ♥ توسط آرش کدخدائی الیادرانی (@slasharash)',
+  mainPage: 'برگه‌ی اصلی',
+  pageNotFound: 'صفحه پیدا نشد',
   postWithThisTag: 'نوشته با این برچسب',
   tagName: (tag: string) => `برچسب «${tag}»`,
   tags: 'برچسب‌ها',
+  youCanVisitFollowigPages: 'شما می‌توانید برگه‌های زیر را مشاهده کنید',
 }
