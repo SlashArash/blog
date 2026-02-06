88bbf4b050c2f4b5560160d71a1ce2450cc9dec6
Arash Kadkhodaei <arash.kadkhodaei@redacreltd.com>
2026-02-06 13:43:54 +0330

    feat: add feed link to the header

--- consts/menu-items.tsx
+++ consts/menu-items.tsx
@@ -1,6 +1,7 @@
 export const menuItems = [
-  { title: "خانه", url: "/" },
-  { title: "بلاگ", url: "/blog/archive" },
-  { title: "برچسب‌ها", url: "/blog/tags" },
-  { title: "آرش", url: "/arash" },
-];
+  { title: 'خانه', url: '/' },
+  { title: 'بلاگ', url: '/blog/archive' },
+  { title: 'برچسب‌ها', url: '/blog/tags' },
+  { title: 'آرش', url: '/arash' },
+  { title: 'فید', url: '/feed.xml' },
+]
